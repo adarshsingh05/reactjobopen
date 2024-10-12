c14353c760889993dcd758f8747440e31552a881
adarsh singh <adarsh.22bce7232@vitapstudent.ac.in>
2024-10-12 08:01:56 +0530

    back changes of port

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
@@ -9,19 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    port: 3000, // Change to the port Render supports
-    host: true,  // Ensure it listens on all interfaces
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id.split("node_modules/")[1].split("/")[0].toString();
-          }
-        },
-      },
-    },
-  },
-});
+})
